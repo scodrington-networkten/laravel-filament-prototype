a93b3155082861298344cfda06a82b66a041062c
simoncodrington <scodrington@networkten.com.au>
2025-07-08 15:22:32 +1000

    changes to project sample

---
+++ resources/views/latest-news.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+    <p>Hello</p>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function(){
+    return ('Hello world');
+});
+
+Route::get('/latest-news', function() {
+    return view('latest-news');
+});
